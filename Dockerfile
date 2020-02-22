2ba57cac1ec4619304668530befc178d530892ab
Niclas Lindstedt <nlindstedt@qkn.se>
2020-02-22 12:28:02 +0100

    Update config & dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,15 @@
 FROM ubuntu:18.04 as build
 ARG DEBIAN_FRONTEND=noninteractive
 WORKDIR /build
-
-# Install prerequisites
 RUN apt-get update && apt-get install -y gcc git libc6-dev make gettext
-
-# Build qtv
 RUN git clone https://github.com/deurk/qtv.git && cd qtv && make
 
 FROM ubuntu:18.04 as run
 ARG DEBIAN_FRONTEND=noninteractive
-
-# Install prerequisites
-RUN apt-get update && apt-get install -y gettext
-
 WORKDIR /qtv
+RUN apt-get update && apt-get install -y gettext \
+  && rm -rf /var/lib/apt/lists/*
 COPY --from=build /build/qtv/qtv.bin ./qtv.bin
-COPY files/qtv.cfg.template ./
+COPY files .
 COPY scripts/entrypoint.sh /
 ENTRYPOINT ["/entrypoint.sh"]
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
 
+error() {
+  echo $1
+  exit 1
+}
+
+echo "Setting defaults..."
+[ ! -s "$HOSTNAME" ] && export HOSTNAME="nQuake KTX Server"
+[ ! -s "$LISTEN_PORT" ] && export LISTEN_PORT=27500
+[ ! -s "$ADMIN_PASSWORD" ] && export ADMIN_PASSWORD="changeme"
+[ ! -s "$QTV_PASSWORD" ] && export QTV_PASSWORD="changeme"
+
 echo "Configuring QTV..."
-envsubst < /qtv/qtv.cfg.template > /qtv/qtv.cfg
+envsubst < /qtv/qtv.cfg.template > /qtv/qtv.cfg || error "Could not configure QTV"
+
+# Add target servers to end of qtv.cfg
+added=0
+servers=$(echo $TARGET_SERVERS | tr "," "\n")
+for server in $servers
+do
+  added=1
+  echo "qtv $server" >> /qtv/qtv.cfg
+done
+[ "$added" = "0" ] && error "No target servers specified"
 
 echo "Starting QTV"
 cd /qtv
-./qtv.bin
+./qtv.bin +exec qtv.cfg || error "Could not start QTV"
